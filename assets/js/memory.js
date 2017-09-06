9319d729584c34dfaeccbb12b645ed9e4647bcb0
Alexandre Douard <kalambik@gmail.com>
2017-09-06 22:09:10 +0200

    modalreset

--- assets/js/memory.js
+++ assets/js/memory.js
@@ -3,35 +3,36 @@ $(document).ready(function(){
     /**
     * Function qui 
     */
+
+
+    
     let Memory = {
-    	init: function(cartes){
-    		//this.carte =  $('.carte');
-    		//this.container = $(".container");
+
+    	init: function(cards){
+    		$("#modal").modal();
+    		//copie de cards
+    		this.cartes = cards.slice(0);
+    		console.log(cards);
     		this.counter = 0;
-    		this.match = false;
-    		this.flip_activated = false;
     		this.deck = [];
     		this.first_card = "";
     		this.second_card = "";
     		this.setup();
     		this.shuffle(this.deck);
-    		//this.shuffle(this.cards);
     		this.flip();
     		this.display();
-    		//this.binding();
-    		//this.paused = false;
     	},
 
     	setup: function(){
 		  	//création du paquet de cartes
-		  	let length = cartes.length;
+		  	let length = this.cartes.length;
 		  	for (var i = 0; i < length; i++) {
           		//premiere carte
-          		let card = cartes[0];
+          		let card = this.cartes[0];
          	    // Pousse 2 copies dans le tableau deck
          	    this.deck.push(card,card);
        	  	    // Supprime le premier élément du tableau cartes
-       	  	    cartes.shift();
+       	  	    this.cartes.shift();
        	  	}; 	
        	  },
 
@@ -50,64 +51,49 @@ $(document).ready(function(){
 		},
 
 		flip: function(){
+
 			$(".container").on('click','.carte', function(){
-				//this.counter = 0;
 				let carte_click = $(this);
-				//Memory.first_card = "";
-			//Memory.second_card = "";
-				//let match = false;
-				
 				if(Memory.counter <=2 && !carte_click.hasClass('flip')){
 					//tourne la carte clickee
 					carte_click.addClass('flip');
 					Memory.counter++;
-					console.log(Memory.counter);
-					//Memory.flip_activated = true;
+					//src premiere carte
 					if(Memory.counter==1){
 						Memory.first_card = $('.carte.flip .back img').attr("src");
-						console.log(Memory.first_card);
-
 					}
+					//src deuxieme carte
 					if(Memory.counter==2){
-						//match=true;
-					//if( Memory.counter == 2 && second_card === ""){
 						Memory.second_card = carte_click.find(".back img").attr("src");
-						console.log(Memory.second_card);
+					//modal
+					let all_cards = $('.carte.flip').length;
+					if(Memory.deck.length == all_cards ){
+						$('#modal').modal({backdrop: 'static', keyboard: false});
 					}
-					
-				} 
-
-
-				if(Memory.counter>2 && Memory.first_card === Memory.second_card){
-					Memory.counter = 1;
-					Memory.first_card ="";
-					Memory.second_card ="";
-					block=true
-                    
-				}
-				//console.log($('.carte img').attr("src"));
-				//console.log($('.carte.front.flip .back img').attr("src"));
-				if(Memory.counter>2 && Memory.first_card !== Memory.second_card){
-					console.log(carte_click.find(".back img").attr("src"));
-					//Memory.flip_activated = false;
-					Memory.counter = 1;
-					//initialise toutes les cartes avec class flip
-					$('.carte').removeClass('flip');
+				}	
+			} 
+                //si les cartes sont identiques
+                if(Memory.counter>2 && Memory.first_card === Memory.second_card){
+                	Memory.counter = 1;
+                	Memory.first_card ="";
+                	Memory.second_card ="";
+                	//ajout de la classe block à toutes les cartes tournées 
+                	$('.carte.flip').addClass('block');
+                	//sauf la derniere (premiere carte au compteur)
+                	carte_click.removeClass('block');
+                }
+                //si les cartes sont differentes
+                if(Memory.counter>2 && Memory.first_card !== Memory.second_card){
+                	//premiere carte
+                	Memory.first_card = carte_click.find(".back img").attr("src");
+                	Memory.counter = 1;
+					//tourne toutes les cartes sans la classe block et avec la classe flip 
+					$('.carte.flip:not(".block")').removeClass('flip');
 					//au troisieme click
 					carte_click.addClass('flip');
-
-					//Memory.counter++;
-						//$('.carte.flip .front img').attr("src")=first_card;
-						//console.log(first_card);
-
-
-
-					}
-
+				}
 				
-
 			});
-			
 		},
 
 		display: function(){
@@ -119,11 +105,9 @@ $(document).ready(function(){
             	}).appendTo(".container");
             }
         },
-
 };// end memory
 
-
-let cartes = [
+let cards = [
 "assets/images/heart.png",
 "assets/images/disquette.png",
 "assets/images/malette.png",
@@ -134,26 +118,12 @@ let cartes = [
 "assets/images/clock.png",
 ];
 
-       //console.log( deck);
-       //console.log( cartes);
-
-
-    //let carte = $('.carte');
-    //let container = $(".container");
-    //let counter = 0;
-    //let flip = false;
-
-
-    Memory.init(cartes);
-
-
-
-
-
- //toggle "carte" to "cart flip"
-   // $(".container").on('click','.carte', function(){
-    //	$(this).toggleClass('flip', '');	
-   // });
-   
+$("button").click(function() {
+	$(".carte").remove();
+	$('#modal').modal('hide');
+	Memory.init(cards);
+	console.log(cards);
+});
+Memory.init(cards);
 });
 
